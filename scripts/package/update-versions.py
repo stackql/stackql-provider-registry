c6178a0b8fe240268df2ac492eac4327a174aa32
jeffreyaven <javen@avensolutions.com>
2023-01-02 12:36:47 +1100

    added google

--- scripts/package/update-versions.py
+++ scripts/package/update-versions.py
@@ -9,13 +9,14 @@ providers = json.loads(os.getenv('PROVIDERS'))
 # update versions globally in the provider.yaml for each provider
 for provider in providers:
     provider_name = provider["provider"]
+    provider_dir = provider["provider_dir"]
     source_version = provider["source_version"]
     target_version = provider["target_version"]
 
     print("updating %s from %s to %s" % (provider_name, source_version, target_version))
 
     # update the provider.yaml
-    provider_yaml = "providers/src/%s/%s/provider.yaml" % (provider_name, source_version)
+    provider_yaml = "providers/src/%s/%s/provider.yaml" % (provider_dir, source_version)
     with FileInput(files=[provider_yaml], inplace=True) as f:
         for line in f:
             op = line.replace(source_version, target_version)
