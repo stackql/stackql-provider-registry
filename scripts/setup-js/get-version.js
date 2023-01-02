138a0a42c1a8a564f5c967fc936558c62da7002b
jeffreyaven <javen@avensolutions.com>
2023-01-02 13:06:58 +1100

    updated version to use dots

--- scripts/setup-js/get-version.js
+++ scripts/setup-js/get-version.js
@@ -1,6 +1,6 @@
 import { exec } from 'node:child_process';
 
-const version = `v${process.env['REG_COMMIT_YEAR']}-${process.env['REG_COMMIT_MONTH']}-${process.env['REG_PR_NO'].padStart(5, '0')}`;
+const version = `v${process.env['REG_COMMIT_YEAR']}.${process.env['REG_COMMIT_MONTH']}.${process.env['REG_PR_NO'].padStart(5, '0')}`;
 
 console.log(`REG_VERSION: ${version}`);
 
